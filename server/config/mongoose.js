c7f856199725d50af05ff59f447d8543e79e354f
kephin <wunshon76325@gmail.com>
2017-04-22 23:59:18 +0800

    capture the termination events

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -16,3 +16,28 @@ mongoose.connection.on('error', err => {
 mongoose.connection.on('disconnected', () => {
   console.log('Mongoose disconnected.');
 });
+
+const gracefulShutdown = (msg, callback) => {
+  mongoose.connection.close(() => {
+    console.log('Mongoose disconnected through ' + msg);
+    callback();
+  });
+};
+// For nodemon restarts
+process.once('SIGUSR2', () => {
+  gracefulShutdown('nodemon restart', () => {
+    process.kill(process.pid, 'SIGUSR2');
+  });
+});
+// For app termination
+process.on('SIGINT', () => {
+  gracefulShutdown('app termination', () => {
+    process.exit(0);
+  });
+});
+// For Heroku app termination
+process.on('SIGTERM', () => {
+  gracefulShutdown('Heroku app termination', () => {
+    process.exit(0);
+  });
+});
