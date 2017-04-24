61b0fb67f5a585b2342b415624d44517d853c97c
kephin <wunshon76325@gmail.com>
2017-04-24 18:15:26 +0800

    move config and models to api

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
-require('./server/config/mongoose');
+require('./api/config/mongoose');
 const routes = require('./server/routes/index');
 
 const app = express();
