c86a6b1beefc13cd9ece7e6fd490adb365716f77
kephin <wunshon76325@gmail.com>
2017-04-24 14:07:21 +0800

    remove users route

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
 require('./server/config/mongoose');
-const index = require('./server/routes/index');
-const users = require('./server/routes/users');
+const routes = require('./server/routes/index');
 
 const app = express();
 
@@ -23,8 +22,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
