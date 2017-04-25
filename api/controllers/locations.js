759de4f2d3c1312ecf88431a518398b7a49b6ca0
kephin <wunshon76325@gmail.com>
2017-04-25 22:40:20 +0800

    add locationsListByDistance controller

--- api/controllers/locations.js
+++ api/controllers/locations.js
@@ -4,7 +4,33 @@ const Location = require('../models/location');
 
 module.exports = {
   async locationsListByDistance(req, res) {
+    const { lng, lat, maxDistance } = req.query;
+    try {
+      const locations = await Location
+        .geoNear({
+          type: 'Point',
+          coordinates: [parseFloat(lng), parseFloat(lat)],
+        }, {
+          spherical: true,
+          num: 10,
+          maxDistance: parseFloat(maxDistance),
+        });
 
+      const outputLocations = locations
+        .map(location => ({
+          _id: location.obj._id,
+          name: location.obj.name,
+          address: location.obj.address,
+          facilities: location.obj.facilities,
+          distance: location.dis,
+          ratingAverage: location.obj.ratingAverage,
+          numOfReviews: location.obj.numOfReviews,
+        }));
+
+      res.status(200).json(outputLocations);
+    } catch (err) {
+      res.status(404).json(err);
+    }
   },
   async locationsCreate(req, res) {
 
