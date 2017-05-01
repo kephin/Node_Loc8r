339930618f73ea428dda2f9c7e5463dc2b9a50a3
kephin <wunshon76325@gmail.com>
2017-05-01 20:24:09 +0800

    update review controller by calling the api

--- server/controllers/locations.js
+++ server/controllers/locations.js
@@ -30,51 +30,25 @@ module.exports = {
     });
 
   },
-  review(req, res, next) {
+  async review(req, res, next) {
+    const response = await axios.get(`${apiOptions.server}/api/locations/${req.params.locationId}`);
+
+    const locationDetail = response.data;
+    locationDetail.coords = {
+      lng: response.data.geometry.coordinates[0],
+      lat: response.data.geometry.coordinates[1],
+    };
+
     res.render('location-info', {
-      title: 'Starcups',
+      title: locationDetail.name,
       pageHeader: {
-        title: 'Starcups'
+        title: locationDetail.name,
       },
       sidebar: {
         context: 'is on Loc8r because it has accessible wifi and space to sit down with your laptop and get some work done.',
         callToAction: 'If you\'ve been and you like it - or if you don\'t - please leave a review to help other people just like you.'
       },
-      location: {
-        name: 'Starcups',
-        address: '125 High Street, Reading, RG6 1PS',
-        rating: 3,
-        facilities: ['Hot drinks', 'Food', 'Premium wifi'],
-        coords: {
-          lat: 51.455041,
-          lng: -0.9690884
-        },
-        openingTimes: [{
-          days: 'Monday - Friday',
-          opening: '7:00am',
-          closing: '7:00pm',
-          closed: false
-        }, {
-          days: 'Saturday',
-          opening: '8:00am',
-          closing: '5:00pm',
-          closed: false
-        }, {
-          days: 'Sunday',
-          closed: true
-        }],
-        reviews: [{
-          author: 'Simon Holmes',
-          rating: 5,
-          timestamp: '16 July 2013',
-          reviewText: 'What a great place.\nI can\'t say enough good things about it.'
-        }, {
-          author: 'Charlie Chaplin',
-          rating: 3,
-          timestamp: '16 June 2013',
-          reviewText: 'It was okay. Coffee wasn\'t great, but the wifi was fast.'
-        }]
-      }
+      location: locationDetail,
     });
   },
   createComment(req, res, next) {
