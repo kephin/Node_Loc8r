cd658bb9f85e03fef61a6b5348bd4d0750c1e41a
kephin <wunshon76325@gmail.com>
2017-05-03 19:03:24 +0800

    catch status-specific error

--- server/controllers/locations.js
+++ server/controllers/locations.js
@@ -29,29 +29,35 @@ module.exports = {
       locations: response.data,
       message,
     });
-
   },
   async review(req, res, next) {
-    const response = await axios.get(`${apiOptions.server}/api/locations/${req.params.locationId}`);
-
-    const locationDetail = response.data;
-    locationDetail.coords = {
-      lng: response.data.geometry.coordinates[0],
-      lat: response.data.geometry.coordinates[1],
-    };
-
-    res.render('location-info', {
-      title: locationDetail.name,
-      pageHeader: {
+    try {
+      const response = await axios.get(`${apiOptions.server}/api/locations/${req.params.locationId}`);
+      const locationDetail = response.data;
+      locationDetail.coords = {
+        lng: response.data.geometry.coordinates[0],
+        lat: response.data.geometry.coordinates[1],
+      };
+      res.render('location-info', {
         title: locationDetail.name,
-      },
-      sidebar: {
-        context: 'is on Loc8r because it has accessible wifi and space to sit down with your laptop and get some work done.',
-        callToAction: 'If you\'ve been and you like it - or if you don\'t - please leave a review to help other people just like you.'
-      },
-      location: locationDetail,
-      moment,
-    });
+        pageHeader: {
+          title: locationDetail.name,
+        },
+        sidebar: {
+          context: 'is on Loc8r because it has accessible wifi and space to sit down with your laptop and get some work done.',
+          callToAction: 'If you\'ve been and you like it - or if you don\'t - please leave a review to help other people just like you.'
+        },
+        location: locationDetail,
+        moment,
+      });
+    } catch (err) {
+      // all 4XX - 6XX resopnse will be considered in reject
+      const { status, statusText } = err.response;
+      res.status(status).render('generic-text', {
+        title: `${status}, ${statusText}`,
+        content: err.response.data.message || 'Something goes wrong!',
+      });
+    }
   },
   createComment(req, res, next) {
     res.render('location-review-form', {
