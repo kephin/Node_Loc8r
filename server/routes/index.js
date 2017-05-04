b07616e9dbdf3114e45a4fcb54a71a15cdffc718
kephin <wunshon76325@gmail.com>
2017-05-04 21:59:43 +0800

    update review routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const ctrlLocations = require('../controllers/locations');
+const ctrlReviews = require('../controllers/reviews');
 const ctrlOthers = require('../controllers/others');
 
 /* Location pages */
 router.get('/', ctrlLocations.index);
 router.get('/location/:locationId', ctrlLocations.review);
-router.get('/location/review/new', ctrlLocations.createComment);
+
+/* Review pages */
+router.get('/location/:locationId/reviews/new', ctrlReviews.createComment);
+router.post('/location/:locationId/reviews/new', ctrlReviews.execCreateComment);
 
 /* Other pages */
 router.get('/about', ctrlOthers.about);
