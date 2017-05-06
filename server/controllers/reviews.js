97635783393faeaa5f565ff9a09a5efff72eb0af
kephin <wunshon76325@gmail.com>
2017-05-06 20:28:43 +0800

    validate at the application level with Express

--- server/controllers/reviews.js
+++ server/controllers/reviews.js
@@ -22,12 +22,16 @@ module.exports = {
     }
   },
   async execCreateComment(req, res, next) {
+    const postData = {
+      author: req.body.name,
+      rating: parseInt(req.body.rating, 10),
+      reviewText: req.body.review,
+    };
+    if (!postData.author || !postData.rating || !postData.reviewText) {
+      return res.redirect(`/location/${req.params.locationId}/reviews/new?err=validation`);
+    }
     try {
-      await axios.post(`${process.env.SERVER}/api/locations/${req.params.locationId}/reviews`, {
-        author: req.body.name,
-        rating: parseInt(req.body.rating, 10),
-        reviewText: req.body.review,
-      });
+      await axios.post(`${process.env.SERVER}/api/locations/${req.params.locationId}/reviews`, postData);
       res.status(201).redirect(`/location/${req.params.locationId}`);
     } catch (err) {
       if (err.response.status === 400 && err.response.data.name === 'ValidationError') {
